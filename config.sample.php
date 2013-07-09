8a5d23169977059d8d4d9bf601cadff887806723
Henrik Carlsson <henrik@henrikcarlsson.se>
2013-07-09 13:40:16 +0200

    Added a config file for db credentials.

---
+++ config.sample.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	  * Define the configuration constants.
+	  * This file should be renamed config.php
+	  * when *Vesper Lynd* is installed.
+	  *
+	 **/
+
+
+	// The database credentials
+	DEFINE ( 'db_name', 'db_name' );
+	DEFINE ( 'db_user', 'db_user' );
+	DEFINE ( 'db_password', 'db_password' );
+	DEFINE ( 'db_host', 'localhost' );
+?>
